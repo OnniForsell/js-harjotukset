fa938fe26c3d0520a7ad9f85a36a91a224816197
onnif <onni.forsell@gmail.com>
2022-11-27 21:54:49 +0200

    Moduuli 3

--- module3-starters-main/t9/9.js
+++ module3-starters-main/t9/9.js
@@ -1,13 +1,8 @@
 function calculate(){
-    let x = document.getElementById("calculation").value;
-    let num_array = x.split("")
-    let calculation = ''
-
-num_array.forEach((i) => calculation += i);
-let result = Function('return ' + calculation)();
-document.getElementById("result").innerHTML = result;
+    let calculation = document.getElementById("calculation").value;
+    let result = Function('return ' + calculation)();
+    document.getElementById("result").innerHTML = result;
 }
-
 
 const button = document.querySelector('button');
 button.onclick = calculate;
