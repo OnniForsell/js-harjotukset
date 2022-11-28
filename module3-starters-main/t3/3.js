381c66b6806159a4d43bae49621d2f48688204c7
onnif <onni.forsell@gmail.com>
2022-11-28 13:11:48 +0200

    Moduuli 3

--- module3-starters-main/t3/3.js
+++ module3-starters-main/t3/3.js
@@ -1,2 +1,8 @@
 'use strict';
 const names = ['John', 'Paul', 'Jones'];
+
+let list = document.getElementById('target');
+for (let i = 0; i !== names.length; i++){
+  list.innerHTML += '<li>' + names[i] + '</li>'
+  console.log(names[i])
+}
