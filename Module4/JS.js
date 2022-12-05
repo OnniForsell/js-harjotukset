e208c65d180944ed0c464bd2ef1365328c7a5935
onnif <onni.forsell@gmail.com>
2022-12-05 23:35:55 +0200

    Moduuli 4

--- Module4/JS.js
+++ Module4/JS.js
@@ -54,18 +54,29 @@ async function getShow(name){
                     itemImg.alt = 'alt'
                 }
 
-            let itemFig = document.createElement('a');
-            itemFig.innerText = 'Link to details'
-            itemFig.href = data[i].show.url;
-            itemFig.target = '_blank'
+            let itemLink = document.createElement('a');
+            itemLink.innerText = 'Link to details'
+            itemLink.href = data[i].show.url;
+            itemLink.className = 'myBtn';
+
+            itemLink.addEventListener('click', function (){
+                event.preventDefault()
+                iframe.src = data[i].show.url;
+                modal.style.display = "block";
+})
 
             let itemCaption = document.createElement('p');
             itemCaption.innerHTML = data[i].show.summary;
 
             let itemGenres = document.createElement('p');
             let genreList = "| ";
-            for (let g = 0; g !== data[i].show.genres.length; g++){
-                genreList += data[i].show.genres[g] + ' | '
+
+            if (data[i].show.genres.length > 0) {
+                for (let g = 0; g !== data[i].show.genres.length; g++) {
+                    genreList += data[i].show.genres[g] + ' | '
+                }
+            } else{
+                genreList = '| No Genre |'
             }
             itemGenres.innerHTML = genreList;
 
@@ -74,8 +85,9 @@ async function getShow(name){
             item.appendChild(itemImg);
             item.appendChild(itemCaption);
             item.appendChild(itemGenres);
-            item.appendChild(itemFig);
+            item.appendChild(itemLink);
             infoArea.appendChild(item);
+
             }
         }
         else{
@@ -106,3 +118,26 @@ button.addEventListener('click', async function(event){
         infoArea.innerHTML = ''
     }
 });
+
+// Get the modal
+const modal = document.getElementById("myModal");
+const iframe = document.getElementById("my-frame");
+
+// Get the button that opens the modal
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+}
